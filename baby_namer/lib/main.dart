65457fdd0dde9d840f175669caf67ce8f6d6e9b2
Scott Nesham <sanesham@gmail.com>
2018-06-05 16:49:27 -0500

    add some prettiness and another test name

--- baby_namer/lib/main.dart
+++ baby_namer/lib/main.dart
@@ -15,11 +15,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return new ListTile(
+      key: new ValueKey(document.documentID),
+      title: new Container(
+        decoration: new BoxDecoration(
+          border: new Border.all(color: const Color(0x80000000)),
+          borderRadius: new BorderRadius.circular(5.0)
+        ),
+        padding: const EdgeInsets.all(10.0),
+        child: new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new Text(document['name']),
+            ),
+            new Text(
+              document['votes'].toString(),
+            )
+          ],
+        ),
+      ),
+      onTap: () => Firestore.instance.runTransaction((transaction) async {
+        DocumentSnapshot freshSnap =
+          await transaction.get(document.reference);
+        await transaction.update(
+          freshSnap.reference, {'votes': freshSnap['votes'] + 1});
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,11 +60,9 @@ class MyHomePage extends StatelessWidget {
             return new ListView.builder(
               itemCount: snapshot.data.documents.length,
               padding: const EdgeInsets.only(top: 10.0),
-              itemExtent: 25.0,
-              itemBuilder: (context, index) {
-                DocumentSnapshot ds = snapshot.data.documents[index];
-                return new Text(" ${ds['name']} ${ds['votes']}");
-              }
+              itemExtent: 55.0,
+              itemBuilder: (context, index) =>
+                _buildListItem(context, snapshot.data.documents[index])
             );
           }),
     );
